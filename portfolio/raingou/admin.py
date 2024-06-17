db7307e1e4ecbc1893e2d536d4cf50f0c2d6e964
Raingou-Ngouyap-Herbert <ngouyapjordany95@gmail.com>
2024-06-17 21:56:35 +0200

    Raingou customized the admin-site

--- portfolio/raingou/admin.py
+++ portfolio/raingou/admin.py
@@ -3,7 +3,24 @@ from .models import *
 # Register your models here.
 
 admin.site.register(User)
-admin.site.register(RaingouProject)
-admin.site.register(RaingouSkill)
+# admin.site.register(RaingouProject)
+# admin.site.register(RaingouSkill)
+
+
+@admin.register(RaingouProject)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'description', 'techonolies_use')
+    search_fields = ('title', 'description')
+    list_filter = ('title',  'user')
+
+@admin.register(RaingouSkill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'user')
+    search_fields = ('name', 'category')
+    list_filter = ('name',  'user')
+
+admin.site.site_header = "Portfolio Administration"
+admin.site.site_title = "Portfolio Admin Portal"
+admin.site.index_title = "Welcome to Portfolio Administration"
 
 
